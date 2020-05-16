b5a344f4cf21122154117c9a0e3ca5853da67f8a
r-wells <r.l.wells@outlook.com>
2020-05-16 14:11:53 -0700

    init commit

---
+++ src/components/Display/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Display extends Component {
+  state = {};
+  render() {
+    return <div>Display Component</div>;
+  }
+}
+
+export default Display;
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Dropdown extends Component {
+  state = {};
+  render() {
+    return <p>Dropdown</p>;
+  }
+}
+
+export default Dropdown;
